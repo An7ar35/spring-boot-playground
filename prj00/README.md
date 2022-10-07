d02cc16ac1110a479ccb29d3a81ff6d4661cb37a
An7ar35 <eadavison@protonmail.com>
2022-10-07 12:47:19 +0200

    Proj00 - "Building an Application with Spring Boot"

---
+++ prj00/README.md
@@ -0,0 +1,3 @@
+# Project 00: Hello, world!
+
+Basic "hello world" REST API based on Spring's [Building an Application with Spring Boot](https://spring.io/guides/gs/spring-boot/) introductory example.
---
+++ prj00/src/test/java/com/ead/playground/spring/prj00/Prj00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.playground.spring.prj00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
