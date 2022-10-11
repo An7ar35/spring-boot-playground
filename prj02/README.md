326c696cf8009b47a3fd963bd2689bcd4def379b
An7ar35 <eadavison@protonmail.com>
2022-10-11 21:19:06 +0200

    Proj02 - "Building a RESTful Web Service"

---
+++ prj02/README.md
@@ -0,0 +1,3 @@
+# Project 02: REST-ful Hello, world! service
+
+Basic "hello world" REST-ful service based on Spring's [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/) introductory example.
---
+++ prj02/src/test/java/com/ead/playground/spring/prj02/Prj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.playground.spring.prj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
